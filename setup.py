0477e577b990d40c527926936bf8445cd41b5cbb
Josef Kulovany <stealthmachines@gmail.com>
2025-07-18 03:34:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='base4096',
     version='2.0',
-    packages=find_packages(),
+    packages=find_packages(),  # Finds the base4096/ package automatically
     include_package_data=True,
-    package_data={
-        '': ['*.py'],
-    },
     description='Base4096 encoding and decoding functions',
     author='Josef Kulovany',
     author_email='charg.chg.wecharg@gmail.com',
@@ -17,7 +14,7 @@ setup(
     install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: ZCHG.ORG LICENSE - https://zchg.org/t/legal-notice-copyright-applicable-ip-and-licensing-read-me/440',
+        'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
     ],
 )
